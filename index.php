78d9a2914c856945a33fe07777f075e577c1a5dd
g1obu1eb1eu <147941528+g1obu1eb1eu@users.noreply.github.com>
2025-04-09 23:28:09 +0200

    suppression

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'controller/pageController.php';
-
-$page = isset($_GET['action']) ? $_GET['action'] : 'home';
-
-$controller = new PageController();
-$controller->renderPage($page);
-?>
